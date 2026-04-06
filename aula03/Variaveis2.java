cab6d1b0b91671b1a41c958c7d6de9781181d5bb
Rafael Ramos da Costa <rafaelramoscosta2208@gmail.com>
2026-04-06 09:28:45 -0300

    Criou a pasta "aula03" e continuou com o aprendizado em Variáveis

---
+++ aula03/Variaveis2.java
@@ -0,0 +1,11 @@
+package aula03;
+
+public class Variaveis2 {
+    public static void main(String[] args) {
+        int myNum;
+        myNum = 15;
+        String msgTeste;
+        msgTeste = "Oie";
+        System.out.println(myNum + " " + msgTeste);
+    }
+}
