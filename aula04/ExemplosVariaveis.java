89307e642d2577111835c2203f55b5d74f53672a
Rafael Ramos da Costa <rafaelramoscosta2208@gmail.com>
2026-04-13 09:22:08 -0300

    Adiciona a classe ExemplosVariaveis com exemplos de variáveis em Java

---
+++ aula04/ExemplosVariaveis.java
@@ -0,0 +1,14 @@
+package aula04;
+
+public class ExemplosVariaveis {
+    public static void main(String[] args) {
+        int num1 = 10;
+        float num2 = 3e4f;
+        double num3 = 4e4d;
+        char money = '$';
+
+        System.out.println(num1 * (num2 + num3) + " " + money);
+
+    }
+    
+}
