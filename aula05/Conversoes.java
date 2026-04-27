7d42cd0ee84bce8de7bec7903b4294f3a8fb0b25
Rafael Ramos da Costa <rafaelramoscosta2208@gmail.com>
2026-04-27 09:30:15 -0300

    Adiciona as classes Conversoes e Operadores com exemplos de conversão e operações em Java

---
+++ aula05/Conversoes.java
@@ -0,0 +1,18 @@
+package aula05;
+public class Conversoes {
+    public static void main(String[] args) {
+        // Automatic (widening)
+        int myInt = 9;
+        double myDouble = myInt; // Automatic casting: int to double
+
+        System.out.println(myInt);    // Outputs 9
+        System.out.println(myDouble); // Outputs 9.0
+
+        //Manual (narrowing) Casting in Java
+        double myDouble2 = 9.78d;
+        int myInt2 = (int) myDouble2; // Manual casting: double to int
+
+        System.out.println(myDouble2); // Outputs 9.78
+        System.out.println(myInt2);    // Outputs 9
+    }
+}
---
+++ aula05/Operadores.java
@@ -0,0 +1,19 @@
+package aula05;
+public class Operadores {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 3;
+
+        System.out.println(x + y); // 13
+        System.out.println(x - y); // 7
+        System.out.println(x * y); // 30
+        System.out.println(x / y); // 3
+        System.out.println(x % y); // 1
+
+        int z = 5;
+        ++z; //Incrementa +1 a z
+        System.out.println(z); // 6
+        --z; //Decrementa -1 a z
+        System.out.println(z); // 5
+    }
+}
