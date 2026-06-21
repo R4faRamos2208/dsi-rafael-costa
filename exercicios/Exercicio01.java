8a2cbc75517563ee1dfdd13309d4ccd8dd708653
Rafael Ramos da Costa <rafaelramoscosta2208@gmail.com>
2026-06-21 20:18:32 -0300

    Atualiza Exercicio01 para calcular descontos de INPS e IR, e exibir salário líquido com adicionais

--- exercicios/Exercicio01.java
+++ exercicios/Exercicio01.java
@@ -6,7 +6,8 @@ public class Exercicio01 {
         Scanner input = new Scanner(System.in);
 
         //Declaração Variaveis
-        double salarioPosIR;
+        double ir = 0;
+        double adicional = 0; //Tive que inicializa senão não rodava :(
 
         //Inserindo nome do funcionário
         System.out.println("Insira o nome do funcionário: ");
@@ -16,7 +17,7 @@ public class Exercicio01 {
         System.out.println("Insira a quantidade de horas trabalhadas no mês: ");
         int horasTrabalhadas = input.nextInt();
 
-        //Inserindo valor rexebido por hora
+        //Inserindo valor recebido por hora
         System.out.println("Insira o valor recebido por HORA: ");
         double valorPorHora = input.nextDouble();
 
@@ -37,42 +38,56 @@ public class Exercicio01 {
         double salarioPorFilho = input.nextDouble();
 
         //Calculando salário bruto
-        double salarioBruto =  (horasTrabalhadas * valorPorHora) + (quantFilhos * salarioPorFilho);
+        double salarioBruto = horasTrabalhadas * valorPorHora;
         System.out.println("Seu salário bruto é: " + salarioBruto);
 
-        //Calculando desconto do INPS
-        double salarioDescontoInps = salarioBruto - (8.5/100);
-        System.out.println("Seu salário após desconto do INPS é : " + salarioDescontoInps);
+        //Calculando salário família
+        double salarioFamilia = quantFilhos * salarioPorFilho;
+        System.out.println("Seu salário família é: " + salarioFamilia);
 
-        //Calculando Imposto de Renda
-        if (salarioDescontoInps > 1500) {
-            salarioPosIR = salarioDescontoInps - salarioDescontoInps * (15/100);
-        }
-        else if (salarioDescontoInps > 500) {
-            salarioPosIR = salarioDescontoInps - salarioDescontoInps * (8/100);
-        }
-        else {
-            salarioPosIR = salarioDescontoInps;
-        }
+        //Calculando desconto do INPS (8,5% do salário bruto)
+        double descontoInps = salarioBruto * 0.085;
+        System.out.println("Seu desconto do INPS é: " + descontoInps);
 
-        System.out.println("Seu salário após descontos do IR é: " + salarioPosIR);
-        
+        //Calculando Imposto de Renda (com base no salário bruto)
+        if (salarioBruto > 1500) {
+            ir = salarioBruto * 0.15;
+        }
+        if (salarioBruto > 500 && salarioBruto <= 1500) {
+            ir = salarioBruto * 0.08;
+        }
+        System.out.println("Seu desconto de IR é: " + ir);
+
         //Calcular Adicionais
 
         //Adicional de idade
-        if (idade>40){
-            salarioPosIR += salarioBruto * (2/100);
+        if (idade > 40) {
+            adicional += salarioBruto * 0.02;
         }
 
         //Adicional tempo de serviço
         if (tempoContribuicao > 15) {
-            salarioPosIR += salarioBruto * (3.5/100);
-        } 
-        else if (tempoContribuicao > 5 || idade > 30) {
-            salarioPosIR += salarioBruto * (1.5/100);
+            adicional += salarioBruto * 0.035;
         }
-        
+        if (tempoContribuicao <= 15 && tempoContribuicao > 5 && idade > 30) {
+            adicional += salarioBruto * 0.015;
+        }
 
-   }    
-    
+        //Total de descontos
+        double totalDescontos = descontoInps + ir;
+
+        //Salário líquido
+        double salarioLiquido = salarioBruto - totalDescontos + adicional + salarioFamilia;
+
+        //Resultados
+        System.out.println("Informações:");
+        System.out.println("Funcionário: " + nomeFunc);
+        System.out.println("Salário bruto: " + salarioBruto);
+        System.out.println("Total de descontos: " + totalDescontos);
+        System.out.println("Adicionais: " + adicional);
+        System.out.println("Salário líquido: " + salarioLiquido);
+
+         input.close();
+   }
+
 }
